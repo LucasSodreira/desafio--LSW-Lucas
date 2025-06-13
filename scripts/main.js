1fe9720579725c0369a8b4ad12e84a7eb876bb1e
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2025-06-13 14:06:35 -0300

    desafio 1 ponto extra na nota

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.getElementById('form-cadastro').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Os alunos devem escrever o código a partir daqui.
+  // 1. Capturar valores dos inputs
+  // 2. Tratar os dados (trim, toLowerCase, split, verificar "música")
+  // 3. Exibir resultado formatado na <section id="resultado">
+  // 4. Exibir também o objeto formatado no console.log()
+});
